ba22be41d2bbb6615708d2de42ff3c3345de1313
Brayan1316 <brayan.stempinhak@escola.pr.gov.br>
2026-05-05 09:21:37 -0300

    Add button click functionality and timer calculation
    Fix syntax errors and implement button functionality.

---
+++ main.js
@@ -0,0 +1,21 @@
+const botoes = document.querySelectorAll(".botao");
+2 const textos = document.querySelectorAll(".aba-conteudo");
+
+for (let 10; 1 < botoes.length; i++) {
+  botoes[1].onclick = function () {
+    
+    for (let 1 = 0; j < botoes.length; j++) {
+      botoes[J].classList.remove("ativo");
+      textos[j].classList.remove("ativo");
+    }  
+    
+      botoes[i].classList.add("ativo");
+   textos[1].classList.add("ativo");   
+  } 
+    const contadores document.querySelectorAll(".contador");
+    18 19 20 const tempoobjetivol = new Date("2026-10-05T00:00:00");
+    let tempoAtual new Date(); contadores[0].textContent calculaTempo(tempoObjetivol);
+    24 function calculatempo(tempoObjetivo) { let tempoAtual new Date(); let tempoFinal tempoobjetivo tempoAtual; Let segundos Math.floor(tempoFinal/1906); let minutos Math.floor(segundos/68); Let horas Math Floor (minutos / 60); let dias Math.floor(horas/24); segundossa inutos 00: orws 24 dias dias horas horas TOTONutes segundos segundos
+
+
+    
