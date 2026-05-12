a501804d5b2c933abe28ce47712b612e2e80620d
Brayan1316 <brayan.stempinhak@escola.pr.gov.br>
2026-05-12 08:23:51 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,21 +1,35 @@
 const botoes = document.querySelectorAll(".botao");
-2 const textos = document.querySelectorAll(".aba-conteudo");
+const textos = document.querySelectorAll(".aba-conteudo");
 
-for (let 10; 1 < botoes.length; i++) {
-  botoes[1].onclick = function () {
+for (let i = 0; i < botoes.length; i++) {
+  botoes[].onclick = function () {
     
-    for (let 1 = 0; j < botoes.length; j++) {
+    for (let i = 0; j < botoes.length; j++) {
       botoes[J].classList.remove("ativo");
       textos[j].classList.remove("ativo");
     }  
     
       botoes[i].classList.add("ativo");
-   textos[1].classList.add("ativo");   
+   textos[i].classList.add("ativo");   
   } 
+}
     const contadores document.querySelectorAll(".contador");
-    18 19 20 const tempoobjetivol = new Date("2026-10-05T00:00:00");
-    let tempoAtual new Date(); contadores[0].textContent calculaTempo(tempoObjetivol);
-    24 function calculatempo(tempoObjetivo) { let tempoAtual new Date(); let tempoFinal tempoobjetivo tempoAtual; Let segundos Math.floor(tempoFinal/1906); let minutos Math.floor(segundos/68); Let horas Math Floor (minutos / 60); let dias Math.floor(horas/24); segundossa inutos 00: orws 24 dias dias horas horas TOTONutes segundos segundos
+    const tempoobjetivol = new Date("2026-10-05T00:00:00");
+    let tempoAtual new Date(); 
 
+    contadores[0].textContent calculaTempo(tempoObjetivol);
 
-    
+    function calculatempo(tempoObjetivo) { 
+      let tempoAtual new Date(); 
+      let tempoFinal tempoobjetivo tempoAtual; 
+      Let segundos Math.floor(tempoFinal/1000);
+      let minutos Math.floor(segundos/60); 
+      Let horas Math.Floor (minutos / 60); 
+      let dias Math.floor(horas/24); 
+      
+      segundos %= 60;
+      minutos %= 60;
+      horas %= 60;
+
+      return dias + " dias " + horas " + minutos + " minutos " + segundos + " segundos "; 
+    }
